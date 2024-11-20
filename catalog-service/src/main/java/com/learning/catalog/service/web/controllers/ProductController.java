375548512fb15e09990a81ebe9b7279a88b87b3a
Pushpendra Kushvaha <pushpendra7096@gmail.com>
2024-11-20 12:21:56 +0530

    Catalog Service module setup

---
+++ catalog-service/src/main/java/com/learning/catalog/service/web/controllers/ProductController.java
@@ -0,0 +1,36 @@
+package com.learning.catalog.service.web.controllers;
+
+import com.learning.catalog.service.domain.PagedResult;
+import com.learning.catalog.service.domain.Product;
+import com.learning.catalog.service.domain.ProductNotFoundException;
+import com.learning.catalog.service.domain.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+class ProductController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+    private final ProductService productService;
+
+    ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        return productService.getProducts(pageNo);
+    }
+
+    @GetMapping("/{code}")
+    ResponseEntity<Product> getProductByCode(@PathVariable String code) {
+        log.info("Fetching product for code: {}", code);
+        return productService
+                .getProductByCode(code)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> ProductNotFoundException.forCode(code));
+    }
+}

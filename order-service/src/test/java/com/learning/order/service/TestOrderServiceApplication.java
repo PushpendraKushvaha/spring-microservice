5578ba16b7aca4715e3a85b9409d3c61f246065d
Pushpendra Kushvaha <pushpendra7096@gmail.com>
2024-11-26 14:33:07 +0530

    order-service commit

---
+++ order-service/src/test/java/com/learning/order/service/TestOrderServiceApplication.java
@@ -0,0 +1,12 @@
+package com.learning.order.service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(OrderServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
